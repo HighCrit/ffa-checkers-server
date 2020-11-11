0d3576adc49d2a178f0d74e657948b8d0caa8825
HighCrit <35771251+HighCrit@users.noreply.github.com>
2020-11-11 17:33:16 +0100

    Initial Server Setup
    Made an initial setup of both the api and socket module

---
+++ api/src/test/java/com/highcrit/ffacheckers/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.highcrit.ffacheckers.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
