c27dbd4abce09bb723cb7396a24d9d0c415a4b3c
HighCrit <35771251+HighCrit@users.noreply.github.com>
2021-01-06 22:16:15 +0100

    Converted Thread sleep to Awaitility

--- socket/src/test/java/com/highcrit/ffacheckers/socket/server/objects/listeners/OnDisconnectionTest.java
+++ socket/src/test/java/com/highcrit/ffacheckers/socket/server/objects/listeners/OnDisconnectionTest.java
@@ -1,19 +1,21 @@
 package com.highcrit.ffacheckers.socket.server.objects.listeners;
 
+import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.concurrent.TimeUnit;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.highcrit.ffacheckers.socket.lobby.objects.ILobby;
 import com.highcrit.ffacheckers.socket.server.SocketManager;
 import com.highcrit.ffacheckers.socket.server.objects.clients.PlayerClient;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class OnDisconnectionTest {
-  private SocketIOClient socket = mock(SocketIOClient.class);
+  private final SocketIOClient socket = mock(SocketIOClient.class);
   private SocketManager socketManager;
   private OnDisconnection listener;
 
@@ -24,22 +26,20 @@ class OnDisconnectionTest {
   }
 
   @Test
-  void onDisconnect() throws InterruptedException {
+  void onDisconnect() {
     socketManager.registerClient(socket, null);
     PlayerClient client = socketManager.getInfoByClient(socket);
     ILobby lobby = mock(ILobby.class);
     client.setLobby(lobby);
     listener.onDisconnect(socket);
-    Thread.sleep(500); // Wait a second
     verify(lobby).onPlayerDisconnect(client);
   }
 
   @Test
-  void onDisconnectNotInLobby() throws InterruptedException {
+  void onDisconnectNotInLobby() {
     socketManager.registerClient(socket, null);
     listener.onDisconnect(socket);
-    Thread.sleep(500); // Wait a second
-    Assertions.assertNull(socketManager.getInfoByClient(socket));
+    await().atMost(500, TimeUnit.MILLISECONDS).until(() -> socketManager.getInfoByClient(socket) == null);
   }
 
   @Test
